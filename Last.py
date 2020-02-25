53b20a886f83d28d093fccf0dbf02b78eb2568e7
es-park <61371773+es-park@users.noreply.github.com>
2020-02-25 13:51:02 +0900

    Add files via upload

---
+++ Last.py
@@ -0,0 +1,10 @@
+a = {"CAC": 10.0, "ATX": 390.2, "WIG": 1.2}
+# for i, j in a.items():
+val = list(a.values())
+key = list(a.keys())
+for i in range(len(val)) :
+    if val[i] > val[i+1]
+        answer = key[i]
+    else :
+        answer = key[i+1]
+print(answer)
